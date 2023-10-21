c5dba13f5f2e26f664c5a857bbe905673dbcffab
Matt Tengtrakool <matt_tengtrakool@college.harvard.edu>
2023-10-21 15:40:05 -0400

    with globe2

--- components/globe.js
+++ components/globe.js
@@ -1,21 +1,49 @@
-'use client'
+'use client';
 
-import React, { useEffect, useRef } from 'react';
-import Globe from 'globe.gl';  
+import React, { useState, useEffect, useRef } from 'react';
+import Globe from 'react-globe.gl';
 
 function GlobeComponent() {
-  const globeEl = useRef(null);
+  const globeEl = useRef();
+  const [countries, setCountries] = useState({ features: [] });
+  const [altitude, setAltitude] = useState(0.1);
+  const [transitionDuration, setTransitionDuration] = useState(1000);
 
   useEffect(() => {
-    const myGlobe = Globe()   
-      .globeImageUrl('/globe.jpeg')(globeEl.current);  
+    fetch('/path_to_datasets/ne_110m_admin_0_countries.geojson').then(res => res.json())
+      .then(data => {
+        setCountries(data);
 
-    window.addEventListener('resize', () => {
-      myGlobe.width(window.innerWidth).height(window.innerHeight);
-    });
+        setTimeout(() => {
+          setTransitionDuration(4000);
+          setAltitude(() => feat => Math.max(0.1, Math.sqrt(+feat.properties.POP_EST) * 7e-5));
+        }, 3000);
+      });
   }, []);
 
-  return <div ref={globeEl} style={{ width: '100%', height: '100vh' }}></div>;
+  useEffect(() => {
+    globeEl.current.controls().autoRotate = true;
+    globeEl.current.controls().autoRotateSpeed = 0.3;
+
+    globeEl.current.pointOfView({ altitude: 4 }, 5000);
+  }, []);
+
+  return (
+    <Globe
+      ref={globeEl}
+      globeImageUrl="/globe.jpeg"
+      polygonsData={countries.features.filter(d => d.properties.ISO_A2 !== 'AQ')}
+      polygonAltitude={altitude}
+      polygonCapColor={() => 'rgba(200, 0, 0, 0.6)'}
+      polygonSideColor={() => 'rgba(0, 100, 0, 0.15)'}
+      polygonLabel={({ properties: d }) => `
+        <b>${d.ADMIN} (${d.ISO_A2})</b> <br />
+        Population: <i>${Math.round(+d.POP_EST / 1e4) / 1e2}M</i>
+      `}
+      polygonsTransitionDuration={transitionDuration}
+      style={{ width: '100%', height: '100vh' }}
+    />
+  );
 }
 
 export default GlobeComponent;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 'use client'
 
+import React from 'react';
 import dynamic from 'next/dynamic';
 
-const DynamicGlobeComponent = dynamic(() => import('../../components/globe'), {
+const DynamicGlobeComponent = dynamic(() => import('../../components/globe2'), {
   ssr: false
 });
 
